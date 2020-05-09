9016795d31d3fb021a97863c3bdfb775dcb1a8de
unperknown <wnsdud105@gmail.com>
2020-05-09 23:10:18 +0900

    fix: unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:Eliverd/common/theme.dart';
-import 'package:Eliverd/common/string.dart';
-
-import 'package:Eliverd/ui/pages/home.dart';
 import 'package:Eliverd/ui/pages/splash_screen.dart';
 
 void main() {
