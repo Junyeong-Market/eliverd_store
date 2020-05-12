24dfce3753b9926c80d253f5c0ab6e152fd58709
unperknown <wnsdud105@gmail.com>
2020-05-12 21:38:00 +0900

    fix: test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,21 +5,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:Eliverd/main.dart';
 
 void main() {
-  testWidgets('Splash Screen Test', (WidgetTester tester) async {
+  testWidgets('Main Test', (WidgetTester tester) async {
     await tester.pumpWidget(EliverdStore());
 
     expect(find.text('Eliverd'), findsOneWidget);
 
     await tester.pumpAndSettle(const Duration(seconds: 3));
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-
+    expect(find.text('Eliverd'), findsOneWidget);
   });
 }
