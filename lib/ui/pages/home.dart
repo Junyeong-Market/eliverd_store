6aae35241ac7c7a460d3ffff594fcd6e726c7116
unperknown <wnsdud105@gmail.com>
2020-05-17 20:15:03 +0900

    feat: Splash & Login & Main

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:Eliverd/common/string.dart';
+import 'package:Eliverd/common/color.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,12 +9,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+      body: Center(
+        child: Text('엘리버드')
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              title: Text(addProduct),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.payment),
+              title: Text(checkOut),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.remove),
+              title: Text(removeProduct),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: eliverdTappedColor,
+          onTap: _onItemTapped
       ),
     );
   }
--- lib/ui/pages/login.dart
+++ lib/ui/pages/login.dart
@@ -1,4 +1,7 @@
+import 'package:Eliverd/common/string.dart';
 import 'package:flutter/material.dart';
+
+import './home.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,12 +9,61 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final idController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      body: ListView(
+        padding: EdgeInsets.all(height / 15.0),
+        children: <Widget>[
+          TextField(
+            obscureText: false,
+            controller: idController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(15.0),
+              ),
+              labelText: idText,
+            ),
+          ),
+          SizedBox(height: height / 80.0),
+          TextField(
+            obscureText: true,
+            controller: passwordController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(15.0),
+              ),
+              labelText: passwordText,
+            ),
+          ),
+          SizedBox(height: height / 80.0),
+          RaisedButton(
+            child: Text(login),
+            onPressed: () => {
+              // TO-DO: 로그인 BLOC 구현하기
+              Navigator.push(context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage())
+              )},
+            textColor: Colors.white,
+            color: Colors.lightBlue,
+            shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(15.0),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 15.0),
+          ),
+          FlatButton(
+            child: Text(notSignUp),
+            onPressed: () => {
+              // TO-DO: 회원가입 BLOC 구현하기
+            },
+          )
+        ],
       ),
     );
   }
