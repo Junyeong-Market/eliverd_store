7c1168771d561fd00d039d057c1506d4a5b5f7a7
unperknown <wnsdud105@gmail.com>
2020-06-03 17:04:02 +0900

    refactor: enhanced model definition

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,19 +1,39 @@
-class Product {
-  String _id;
-  String _name;
-  String _manufacturer;
-  String _ian;
+import 'package:equatable/equatable.dart';
 
-  String get id => _id;
-  String get name => _name;
-  String get manufacturer => _manufacturer;
-  String get ian => _ian;
+class Product extends Equatable {
+  final String _id;
+  final String _name;
+  final String _manufacturer;
+  final String _ian;
+
+  Product(this._id, this._name, this._manufacturer, this._ian);
+
+  @override
+  List<Object> get props => [ _id, _name, _manufacturer, _ian ];
+
+  @override
+  String toString() {
+    return 'Product { id: $_id, name: $_name, manufacturer: $_manufacturer, ian: $_ian}';
+  }
+
+  Product copyWith({ String id, String name, String manufacturer, String ian }) {
+    return Product(id, name, manufacturer, ian);
+  }
 }
 
-class Manufacturer {
+class Manufacturer extends Equatable {
   String _id;
   String _name;
 
-  String get id => _id;
-  String get name => _name;
+  @override
+  List<Object> get props => [_id, _name];
+
+  @override
+  String toString() {
+    return 'Manufacturer { id: $_id, name: $_name }';
+  }
+  
+  Manufacturer copyWith({ String id, String name }) {
+    return Manufacturer(id, name);
+  }
 }
