4a5749d7141bc725169785c703644c29c79e8a4a
unperknown <wnsdud105@gmail.com>
2020-06-03 20:43:32 +0900

    refactor: color value

--- lib/common/color.dart
+++ lib/common/color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const eliverdLightColor = Color(0xFF09BBFE);
-const eliverdColor = Color(0xFF4F52EE);
-const eliverdDarkColor = Color(0xFF5A42EC);
+const eliverdLightColor = Color(0xFF29F19C);
+const eliverdColor = Color(0xFF03A2F7);
+const eliverdDarkColor = Color(0xFF3973F3);
 const eliverdSwatchColor = Colors.lightBlue;
