3c6f2e3d90c816adae461ca2ba4ad61e413c68a4
unperknown <unperknown@icloud.com>
2020-06-18 15:10:48 +0900

    refactor: classified strings

--- lib/common/string.dart
+++ lib/common/string.dart
@@ -1,56 +1,75 @@
-const title = '엘리버드';
-const homeTitle = '상점용 엘리버드';
+class TitleStrings {
+  static final title = '엘리버드';
+  static final homeTitle = '상점용 엘리버드';
+  static final addProductTitle = '재고 등록';
+  static final updateProductTitle = '재고 수정';
+}
 
-const idText = '아이디';
-const passwordText = '비밀번호';
-const login = '로그인';
-const notSignUp = '아직 회원이 아니십니까?';
+class ErrorMessages {
+  static final realnameInvalidMessage = '이름은 영어나 한글로 128자 이내만 가능합니다.';
+  static final nicknameInvalidMessage = '닉네임은 영어, 한글, 숫자를 조합하여 50자 이내만 가능합니다.';
+  static final nicknameDuplicatedMessage = '이미 존재하는 닉네임입니다. 다른 닉네임을 입력하세요.';
+  static final userIdInvalidMessage = '아이디는 영어, 숫자를 조합하여 50자 이내만 가능합니다.';
+  static final userIdDuplicatedMessage = '이미 존재하는 아이디입니다. 다른 아이디를 입력하세요.';
+  static final passwordInvalidMessage = '비밀번호는 영어, 숫자, 특수문자를 조합하여 256자 이내만 가능합니다.';
+  static final loginErrorMessage = '입력하신 계정은 존재하지 않는 계정입니다. 아이디와 비밀번호를 다시 입력해주세요.';
 
-const loginErrorMessage = '입력하신 계정은 존재하지 않는 계정입니다. 아이디와 비밀번호를 다시 입력해주세요.';
+  static final signUpErrorMessage = '회원가입 요청 중 오류가 발생했습니다. 나중에 다시 시도해주세요.';
+  static final signInErrorMessage = '로그인 요청 중 오류가 발생했습니다. 나중에 다시 시도해주세요.';
+}
 
-const signUp = '회원가입';
-const signUpButtonDesc = '가입하기';
+class SignInStrings {
+  static final idText = '아이디';
+  static final passwordText = '비밀번호';
+  static final login = '로그인';
+  static final notSignUp = '아직 회원이 아니십니까?';
+}
 
-const usernameDescWhenImcompleted = '이름을 입력하세요.';
-const nicknameDescWhenImcompleted = '닉네임을 입력하세요.';
-const idDescWhenImcompleted = '아이디를 입력하세요.';
-const passwordDescWhenImcompleted = '비밀번호를 입력하세요.';
+class SignUpStrings {
+  static final signUp = '회원가입';
+  static final signUpButtonDesc = '가입하기';
 
-const usernameDesc = '이름';
-const nicknameDesc = '닉네임';
-const idDesc = '아이디';
-const passwordDesc = '비밀번호';
-const isSellerDesc = '사업자이신가요?';
+  static final realnameDescWhenImcompleted = '이름을 입력하세요.';
+  static final nicknameDescWhenImcompleted = '닉네임을 입력하세요.';
+  static final idDescWhenImcompleted = '아이디를 입력하세요.';
+  static final passwordDescWhenImcompleted = '비밀번호를 입력하세요.';
 
-const addProductTitle = '재고 등록';
-const updateProductTitle = '재고 수정';
+  static final realnameDesc = '이름';
+  static final nicknameDesc = '닉네임';
+  static final idDesc = '아이디';
+  static final passwordDesc = '비밀번호';
+  static final isSellerDesc = '사업자이신가요?';
+}
 
-const searchProductDesc = '재고 검색';
-const addProductDesc = '재고 등록';
-const updateProductDesc = '재고 수정';
-const checkOutProductDesc = '상품 결제';
-const deleteProductDesc = '재고 폐기';
+class HomePageStrings {
+  static final searchProductDesc = '재고 검색';
+  static final addProductDesc = '재고 등록';
+  static final updateProductDesc = '재고 수정';
+  static final checkOutProductDesc = '상품 결제';
+  static final deleteProductDesc = '재고 폐기';
+}
 
-const priceUndefined = '가격 미정';
+class ProductStrings {
+  static final barcodeDescWhenImcompleted = '바코드가 있다면 등록하세요.';
+  static final nameDescWhenImcompleted = '상품 이름을 입력하세요.';
+  static final priceDescWhenImcompleted = '상품 가격을 입력하세요.';
+  static final manufacturerDescWhenImcompleted = '제조사를 입력하세요.';
 
-const barcodeDescWhenImcompleted = '바코드가 있다면 등록하세요.';
-const nameDescWhenImcompleted = '상품 이름을 입력하세요.';
-const priceDescWhenImcompleted = '상품 가격을 입력하세요.';
-const manufacturerDescWhenImcompleted = '제조사를 입력하세요.';
+  static final barcodeDescWhenUpdate = '바코드 수정 시 클릭하세요.';
+  static final nameDescWhenUpdate = '상품 이름을 수정하세요.';
+  static final priceDescWhenUpdate = '상품 가격을 수정하세요.';
+  static final manufacturerDescWhenUpdate = '제조사를 수정하세요.';
 
-const barcodeDescWhenUpdate = '바코드 수정 시 클릭하세요.';
-const nameDescWhenUpdate = '상품 이름을 수정하세요.';
-const priceDescWhenUpdate = '상품 가격을 수정하세요.';
-const manufacturerDescWhenUpdate = '제조사를 수정하세요.';
+  static final noBarcodeDesc = '바코드 없음';
+  static final barcodeDesc = '바코드';
+  static final nameDesc = '상품 이름';
+  static final priceDesc = '상품 가격';
+  static final manufacturerDesc = '제조사';
 
-const noBarcodeDesc = '바코드 없음';
-const barcodeDesc = '바코드 없음';
-const nameDesc = '상품 이름';
-const priceDesc = '상품 가격';
-const manufacturerDesc = '제조사';
+  static final deleteWarningContent = '한 번 삭제하면 영원히 복구할 수 없습니다.';
+  static final cancel = '취소';
+  static final next = '다음';
+  static final submit = '완료';
+  static final delete = '삭제';
 
-const deleteWarningContent = '한 번 삭제하면 영원히 복구할 수 없습니다.';
-const cancel = '취소';
-const submit = '완료';
-const next = '다음';
-const delete = '삭제';
+}
