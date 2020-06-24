b99f4653cc25e96b4397ca7a9b38df586404b022
unperknown <unperknown@icloud.com>
2020-06-24 17:55:55 +0900

    fix: unused dialog

--- lib/ui/pages/register_store.dart
+++ lib/ui/pages/register_store.dart
@@ -24,57 +24,6 @@ class _RegisterStorePageState extends State<RegisterStorePage> {
   List<User> _registerers = [];
   Coordinate _storeLocation;
 
-  void _openSearchUserDialog() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              brightness: Brightness.light,
-              elevation: 0.0,
-              iconTheme: IconThemeData(color: Colors.black),
-              actions: <Widget>[
-                ButtonTheme(
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  minWidth: 0,
-                  height: 0,
-                  child: FlatButton(
-                    child: Text(
-                      '완료',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 17.0,
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-              ],
-            ),
-            body: ListView(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20.0,
-              ),
-              children: <Widget>[
-                CupertinoTextField(
-                  placeholder: RegisterStoreStrings.registererSearchDesc,
-                  onSubmitted: (value) {
-                    // TO-DO: 사업자 BLOC 구현 후 Search 이벤트 call
-                  },
-                ),
-              ],
-            ),
-          );
-        },
-        fullscreenDialog: true,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
