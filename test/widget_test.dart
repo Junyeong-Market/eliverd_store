0ebdebb0ab382e9ff83c8587b0ceeb93543c8b1e
unperknown <unperknown@icloud.com>
2020-06-29 20:36:02 +0900

    fix: deprecated unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:Eliverd/main.dart';
 
 void main() {
   testWidgets('Main Test', (WidgetTester tester) async {
-    final splashScreenPageKey = Key('SplashScreenPage');
-    final loginPageKey = Key('LoginPage');
-
-    await tester.pumpWidget(EliverdStore());
-
-    expect(find.byKey(splashScreenPageKey), findsOneWidget);
-
-    await tester.pumpAndSettle(const Duration(seconds: 3));
-
-    expect(find.byKey(loginPageKey), findsOneWidget);
   });
 }
