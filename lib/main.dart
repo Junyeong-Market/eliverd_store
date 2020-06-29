b1d058530f4ba1db7a8015a9ce8e0e9a449668dd
unperknown <unperknown@icloud.com>
2020-06-29 20:02:37 +0900

    refactor: make account bloc automatic

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:http/http.dart' as http;
 
 import 'package:Eliverd/bloc/authBloc.dart';
-import 'package:Eliverd/bloc/accountBloc.dart';
 import 'package:Eliverd/bloc/stockBloc.dart';
 
 import 'package:Eliverd/resources/providers/providers.dart';
 import 'package:Eliverd/resources/repositories/repositories.dart';
 
 import 'package:Eliverd/common/theme.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:Eliverd/ui/pages/splash_screen.dart';
 
@@ -36,15 +36,6 @@ class EliverdStore extends StatelessWidget {
             ),
             storeRepository: StoreRepository(
               storeAPIClient: StoreAPIClient(
-                httpClient: http.Client(),
-              ),
-            ),
-          ),
-        ),
-        BlocProvider<AccountBloc>(
-          create: (_) => AccountBloc(
-            accountRepository: AccountRepository(
-              accountAPIClient: AccountAPIClient(
                 httpClient: http.Client(),
               ),
             ),
