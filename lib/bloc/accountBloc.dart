bb7f3a405fd10a6accc1ddec64d97771020cb589
unperknown <unperknown@icloud.com>
2020-06-30 17:41:54 +0900

    refactor: account event redefined

--- lib/bloc/accountBloc.dart
+++ lib/bloc/accountBloc.dart
@@ -18,16 +18,14 @@ class AccountBloc extends Bloc<AccountEvent, AccountState> {
 
   @override
   Stream<AccountState> mapEventToState(AccountEvent event) async* {
-    if (event is AccountCreated) {
-      yield* _mapAccountCreatedToState(event);
-    } else if (event is NewAccountRequested) {
-      yield* _mapNewAccountRequestedToState(event);
-    } else if (event is AccountValidated) {
-      yield* _mapAccountValidatedToState(event);
+    if (event is CreateAccount) {
+      yield* _mapCreateAccountToState(event);
+    } else if (event is ValidateAccount) {
+      yield* _mapValidateAccountToState(event);
     }
   }
 
-  Stream<AccountState> _mapAccountCreatedToState(AccountCreated event) async* {
+  Stream<AccountState> _mapCreateAccountToState(CreateAccount event) async* {
     try {
       if (state is! AccountValidateFailed) {
         await accountRepository.signUpUser(event.jsonifiedUser);
@@ -39,16 +37,13 @@ class AccountBloc extends Bloc<AccountEvent, AccountState> {
     }
   }
 
-  Stream<AccountState> _mapNewAccountRequestedToState(
-      NewAccountRequested event) async* {
-    yield AccountOnCreate();
-  }
-
-  Stream<AccountState> _mapAccountValidatedToState(AccountValidated event) async* {
+  Stream<AccountState> _mapValidateAccountToState(ValidateAccount event) async* {
     try {
+      yield AccountOnCreate();
+
       final validation = await accountRepository.validateUser(event.jsonifiedUser);
 
-      if (!validation.values.every((element) => element == 0)) {
+      if (validation.values.any((value) => value is List)) {
         yield AccountValidateFailed(validation);
       }
     } catch (_) {
