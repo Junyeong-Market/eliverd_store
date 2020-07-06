20f2317d00530afe70ea4684c0705d5f65b56484
unperknown <unperknown@icloud.com>
2020-07-06 15:40:45 +0900

    feat: search user bottom sheet

---
+++ lib/ui/widgets/search_user.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:Eliverd/bloc/events/storeEvent.dart';
+import 'package:Eliverd/bloc/states/storeState.dart';
+import 'package:Eliverd/bloc/storeBloc.dart';
+
+import 'package:Eliverd/models/models.dart';
+
+import 'package:Eliverd/common/color.dart';
+import 'package:Eliverd/common/string.dart';
+
+class SearchUserDialog extends StatefulWidget {
+  @override
+  _SearchUserDialogState createState() => _SearchUserDialogState();
+}
+
+class _SearchUserDialogState extends State<SearchUserDialog> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery
+        .of(context)
+        .size
+        .height;
+
+    return BlocConsumer<StoreBloc, StoreState>(
+      listener: (context, state) {
+        print(state);
+        if (state is StoreLocationRegistered) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        return Container(
+          height: height * 0.8,
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: height / 64.0),
+              Divider(
+                indent: 140.0,
+                endIndent: 140.0,
+                height: 16.0,
+                thickness: 4.0,
+              ),
+              SizedBox(height: height / 64.0),
+              Text(
+                TitleStrings.searchRegisterersTitle,
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 26.0,
+                ),
+              ),
+              Text(
+                SearchSheetStrings.searchRegisterersDesc,
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: height / 48.0),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
