dcb6b71b6320bfeddd04fcc396e273dcfb9e0ca3
unperknown <unperknown@icloud.com>
2020-07-06 20:57:23 +0900

    feat: search user bloc

---
+++ lib/bloc/searchUserBloc.dart
@@ -0,0 +1,30 @@
+import 'package:meta/meta.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:Eliverd/resources/repositories/repositories.dart';
+
+import 'package:Eliverd/bloc/events/searchUserEvent.dart';
+import 'package:Eliverd/bloc/states/searchUserState.dart';
+
+import 'events/searchUserEvent.dart';
+
+class SearchUserBloc extends Bloc<SearchUserEvent, SearchUserState> {
+  final AccountRepository accountRepository;
+
+  SearchUserBloc({@required this.accountRepository})
+      : assert(accountRepository != null);
+
+  @override
+  SearchUserState get initialState => UserNotFound();
+
+  @override
+  Stream<SearchUserState> mapEventToState(SearchUserEvent event) async* {
+    if (event is SearchUser) {
+      final users = await accountRepository.searchUser(event.keyword);
+
+      yield UserFound(users);
+    }
+  }
+
+}
